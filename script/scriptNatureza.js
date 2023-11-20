a6193ec2516038057130a4086de042ddb2741b98
LucasCesarLopes <lucascesar3445@gmail.com>
2023-11-20 13:02:08 -0300

    .

---
+++ script/scriptNatureza.js
@@ -0,0 +1,16 @@
+document.querySelectorAll('.link').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+  
+        const targetId = this.getAttribute('href').substring(1); // Remove o "#" do href
+        const targetElement = document.getElementById(targetId);
+  
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+           
+            });
+        }
+    });
+  });
